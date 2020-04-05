c96ef706c986087c10806c7159fda6e75933cdb1
rameshwar17180 <rameshwar17180@iiitd.ac.in>
2020-04-05 21:13:05 +0530

    lil changes

--- grocery_store.java
+++ grocery_store.java
@@ -1,18 +1,50 @@
 package Connection;
-
+import java.util.*;
 import java.sql.*;
 
 public class grocery_store {
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
 		Mysql_connection mysql_conn=new Mysql_connection();
 		Connection con=mysql_conn.conn("root", "password");
-		try {
-			int k=customer_login("Gurjot123","0123",con);
-			System.out.print(k);
+		System.out.println("welcome to racaj store");
+		System.out.println("want to register: press 1");
+		System.out.println("for log in: press 2");
+		int k=sc.nextInt();
+		int flag=k;
+		while(true) {	
+		Boolean loggedin=false;
+		if(flag==2) {
+			try {
+				System.out.println("Enter login id:");
+				String loginid=sc.next();
+				System.out.println("Enter Password:");
+				String password=sc.next();
+				int log=customer_login(loginid,password,con);
+				if(log==1) {
+					loggedin=true;
+					System.out.println("log in successful");
+					flag=3;
+				}
+				else {
+					loggedin=false;
+					System.out.println("wrong credentials");
+					System.out.println("press 0 to exit or 1 to try again");
+					int ex=sc.nextInt();
+					if(ex==0) break;
+					else continue; 
+				}
+				
+			}
+			catch(Exception e){
+				System.out.println(e);
+			}
 		}
-		catch(Exception e){
-			System.out.println(e);
+		else {
+			break;
 		}
+	}
+		System.out.println("Exit Successful");
 	}
 	static int customer_login(String loginid,String password,Connection con) {
 		try {
